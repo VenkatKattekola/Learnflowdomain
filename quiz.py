5a7740ea026f7571b7d2d2656a965b10d5158dbb
VenkatKattekola <venkateshwarraokattekola@gmail.com>
2024-06-11 15:30:28 +0530

    modified quiz

--- quiz.py
+++ quiz.py
@@ -1,43 +1,43 @@
 Questions={
     "History":{
         "easy":[
-            {"question":"When did India get independence?","answer":"1947"},
-            {"question":"Who is the first prime minister of India?","answer":"Jawaharlal Nehru"},
-            {"question":"Who is known as the father of the nation of India?","answer":"Mahatma Gandhi"}
+            {"question":"When did India get independence? A.1950 B.1947 C.1981 D.1946","answer":"B"},
+            {"question":"Who is the first prime minister of India? A.Jawaharlal nehru B.Rajendra Prasad C.Gandhi D.BG Tilak","answer":"A"},
+            {"question":"Who is known as the father of the nation of India? A.Shivaji B.Mahatma Gandhi C.Rajiv gandhi D.Subhash chadra bose","answer":"B"}
         ],
         "medium":[
-            {"question":"Who is the first person to go to Space?","answer":"Yuri Gagarin"},
-            {"question":"Who is the first Indian to go to Space?","answer":"Rakesh Sharma"},
-            {"question":"Who discovered America?","answer":"Christopher columbus"}
+            {"question":"Who is the first person to go to Space? A.Laika B.Neil Armstrong C.Rakesh Sharma D.Yuri Gagarin","answer":"D"},
+            {"question":"Who is the first Indian to go to Space? A.Sunitha williams B.Kalpana Chawla C.Rakesh Sharma D.Yuri Gagarin","answer":"C"},
+            {"question":"Who discovered America? A.MarcoPolo B.Vincent Van goh C.Galileo D.Christopher columbus","answer":"D"}
         ],
         "hard":[
-            {"question":"In which battle did Napoleon lose?","answer":"Battle of Waterloo"},
-            {"question":"When did the french revolution take place?","answer":"1789-1799"},
-            {"question":"Who is called the iron man of the world?","answer":"Otto von Bismarck"}
+            {"question":"In which battle did Napoleon lose? A.BLood Battle B.Battle of Waterloo C.Battle of strengths D.Sussex Battle","answer":"B"},
+            {"question":"When did the french revolution start? A.1589 B.1689 C.1789 D.1889","answer":"C"},
+            {"question":"Who is called the iron man of the world? A.Mussolini B.Napoleon C.Adolf Hitler D.Otto von bismarck","answer":"D"}
         ]
     },
     "Biology":{
         "easy":[
-            {"question":"What is called the power house of a cell?","answer":"Mitochondria"},
-            {"question":"What is the process thorugh which plants prepare food?","answer":"Photosynthesis"},
-            {"question":"How many sense organs are there in the human body?","answer":"5"}
+            {"question":"What is called the power house of a cell? A.Vacuole B.Membrane C.Ribosomes D.Mitochondria","answer":"D"},
+            {"question":"What is the process thorugh which plants prepare food? A.Respiration B.Transpiration C.Evaporation D.Photosynthesis","answer":"D"},
+            {"question":"How many sense organs are there in the human body? A.5 B.2 C.4 D.6","answer":"A"}
         ],
         "medium":[
-            {"question":"Who discovered pencillin?","answer":"Alexander fleming"},
-            {"question":"Who proposed the theory of evolution?","answer":"Charles darwin"},
-            {"question":"What causes malaria?","answer":"Protozoa"}
+            {"question":"Who discovered pencillin? A.Charles Darwin B.Von Sachs C.Robert Hooke D.Alexander Fleming","answer":"D"},
+            {"question":"Who proposed the theory of evolution? A.Vincent Chansard B.Louis Pasteur C.Charles Darwin D.Aristotle","answer":"C"},
+            {"question":"What causes malaria? A.Bacteria B.Protozoa C.Virus D.Pigs","answer":"B"}
         ],
         "hard":[
-            {"question":"What are the sites for protein synthesis?","answer":"Ribosomes"},
-            {"question":"Which are called the suicidal bags of the cell?","answer":"Lysosomes"},
-            {"question":"Which vitamin helps in blood clotting?","answer":"Vitamin K"}
+            {"question":"What are the sites for protein synthesis? A.Lungs B.Bronchi C.Blood Cells D.Ribosomes","answer":"D"},
+            {"question":"Which are called the suicidal bags of the cell? A.Lysosomes B.Amoeba C.Mitochondria D.Vacuole","answer":"A"},
+            {"question":"Which vitamin helps in blood clotting? A.Vitamin D B.Vitamin A C.Vitamin K D.Vitamin B","answer":"C"}
         ]
     }
 }
 def Ques(question):
     print(question["question"])
-    initial=input("type the answer here: ").strip()
-    if initial.lower()==question["answer"].lower():
+    initial=input("The Option is: ").strip()
+    if initial.upper()==question["answer"].upper():
         print("You are correct")
         return True
     else:
@@ -49,10 +49,10 @@ def subject():
         if sub in Questions:
             return sub
         else:
-            print("subject doesn't exist......")
+            print("Subject doesn't exist......")
 def choose_mode():
     while True:
-        mode=input("choose your mode: ").strip().lower()
+        mode=input("Choose your mode (easy/medium/hard): ").strip().lower()
         if mode in ["easy","medium","hard"]:
             return mode
         else:
